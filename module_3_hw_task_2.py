72ea3f29668715d91826ba70d3200e8dad0fc10a
Alex Tokariev <oleksandr.tokariev@gmail.com>
2025-04-21 18:32:03 +0300

    'added_restrictions_for_min_and_max'

--- module_3_hw_task_2.py
+++ module_3_hw_task_2.py
@@ -2,8 +2,12 @@ import random
 
 def get_numbers_ticket(min: int, max: int, quantity: int) -> list:
     # cheking the correctness of the parameters of the function
-    if (min < 1) | (max > 1000) | quantity > (max - min + 1) :
-        print('Incorrect input data. Check parameters of the function')
+    if (min < 1):
+        print('Incorrect min number. Must be not lower than 1')
+    elif (max > 1000):
+        print('Incorrect max number. Must be not greater than 1000')
+    elif quantity > (max - min + 1) :
+        print(f'Incorrect quantity. Must be between 1 and {max - min + 1}')
     else:
         numbers_set = set()
         while len(numbers_set) < quantity:
@@ -11,5 +15,5 @@ def get_numbers_ticket(min: int, max: int, quantity: int) -> list:
             numbers_set.add(new_number)
         return sorted(list(numbers_set))
 
-lottery_numbers = get_numbers_ticket(1, 49, 6)
+lottery_numbers = get_numbers_ticket(1, 49, 50)
 print("Your lottery numbers:", lottery_numbers)
