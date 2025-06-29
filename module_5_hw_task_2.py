ba9a55150bc48006201481f4cfa2a0b60fba1d89
Alex Tokariev <oleksandr.tokariev@gmail.com>
2025-06-29 13:25:23 +0300

    'module_5_hw_correct_commit'

--- module_5_hw_task_2.py
+++ module_5_hw_task_2.py
@@ -1,13 +1,13 @@
+import re
+
 text = "Загальний дохід працівника складається з декількох частин: 1000.01 як основний дохід, доповнений \
 додатковими надходженнями 27.45 і 324.00 доларів."
 
 def generator_numbers(text: str):
-    for elem in text.split(' '):
-        try:
-            float(elem)
-            yield float(elem)
-        except ValueError:
-            continue
+    pattern = r'\s*\d+(?:\.\d+)?\s*'
+    matches = re.findall(pattern, text)
+    for match in matches:
+        yield float(match)
 
 def sum_profit(text, generator_numbers):
     return sum(list(generator_numbers(text)))
--- module_5_hw_task_4.py
+++ module_5_hw_task_4.py
@@ -3,45 +3,31 @@ def parse_input(user_input):
     cmd = cmd.strip().lower()
     return cmd, *args
 
-def input_error_add_contact(func):
+def input_error(func):
     def inner(*args, **kwargs):
         try:
             return func(*args, **kwargs)
         except ValueError:
-            return "Please enter the name and phone number after the 'add' command."
-    return inner
-
-@input_error_add_contact
-def add_contact(args, contacts):
-    name, phone = args
-    contacts[name] = phone
-    return "Contact added."
-
-def input_error_change_contact(func):
-    def inner(*args, **kwargs):
-        try:
-            return func(*args, **kwargs)
-        except ValueError:
-            return "Please enter the name and phone number after the 'change' command."
-    return inner
-
-@input_error_change_contact
-def change_contact(args, contacts):
-    name, phone = args
-    contacts[name] = phone
-    return "Contact changed."
-
-def input_error_show_phone_number(func):
-    def inner(*args, **kwargs):
-        try:
-            return func(*args, **kwargs)
+            return "Please enter the name and phone number after the 'add' or 'change' command."
         except IndexError:
             return "Please enter the name after the 'phone' command."
         except KeyError:
             return "Please enter a valid name from your contact list"
     return inner
 
-@input_error_show_phone_number
+@input_error
+def add_contact(args, contacts):
+    name, phone = args
+    contacts[name] = phone
+    return "Contact added."
+
+@input_error
+def change_contact(args, contacts):
+    name, phone = args
+    contacts[name] = phone
+    return "Contact changed."
+
+@input_error
 def show_phone_number(args, contacts):
     return f"{args[0]} phone number: {contacts[args[0]]}"
 
